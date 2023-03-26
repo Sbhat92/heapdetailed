eb6035ba8becff97aa73c6b635d2d987d533c497
Sbhat92 <shreyasbhat92@gmail.com>
2023-03-26 16:00:51 -0400

    adding 3 tests tests

--- tests/testall.py
+++ tests/testall.py
@@ -1,2 +1,11 @@
-from heapdetailed import swap_parent, swap_left_child, swap_right_child, move_up, move_down
+# from heapdetailed import swap_parent, swap_left_child, swap_right_child, move_up, move_down
+
+def test_swap_parent():
+    assert swap_parent([1,5,34,3],1) == [1,5,34,3]
+
+def test_swap_left_child():
+    assert swap_left_child([1,5,34,3],1) == [1,3,34,5]
+
+def test_swap_right_child():
+    assert swap_right_child([1,5,34,3,5,6,7],2) == [1,5,6,3,5,34,7]
 
